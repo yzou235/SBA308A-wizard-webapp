51c03fae9817e96be35f99b2ff124e8dba84b7c2
yzou235 <yumingzou235@gmail.com>
2024-01-07 11:52:54 -0500

    finished random spell generator. will start work on Elixir and Wizard Friend random generator.Also want to work on send feedback using post method. not sure if have enough time.

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ let getSpell = () => {
         .then(response => response.json())
         .then(data => {
             let randomIndex = Math.floor(Math.random() * data.length);
-            spellContainer.textContent = `${data[randomIndex].name}`;
+            spellContainer.innerHTML = `
+            <h2 style="color: #FFA500; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5)">${data[randomIndex].name}</h2>
+            <p><strong>Incantation:</strong> ${data[randomIndex].incantation}</p>
+            <p><strong>Effect:</strong> ${data[randomIndex].effect}</p>
+            <p><strong>Type:</strong> ${data[randomIndex].type}</p>
+            <p><strong>Light:</strong> ${data[randomIndex].light}</p>
+            `;
         })
         .catch(error => console.error("Error fetching spell:", error));
 }
